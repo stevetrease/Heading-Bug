c4ba89cabc5ce34280083b6d211d3a7835ff153a
Steve <stevetrease@gmail.com>
2019-02-19 12:54:55 +0000

    Initial Commit

---
+++ Heading Bug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Heading Bug
+//
+//  Created by Steve on 19/02/2019.
+//  Copyright © 2019 Steve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
